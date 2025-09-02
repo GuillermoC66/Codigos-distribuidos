173007ddd889b54df7241f70a65f92d0557ecdaa
GuillermoC66 <72630022+GuillermoC66@users.noreply.github.com>
2025-09-02 01:20:44 +0000

    .fg

--- CarrilloRodriguezGuillermoTarea2.c
+++ CarrilloRodriguezGuillermoTarea2.c
@@ -9,22 +9,22 @@ int main (int argc, char** argv){
    	 
 	MPI_Comm_size(MPI_COMM_WORLD, &size); 
 	if (rank==0)
-	 
 	{ 
 		int mensaje=42; 
 		
-	 	printf("Proceso %d envia mensaje %d  al proceso 1 ", rank,mensaje); 
-	 	MPI_Send(&mensaje, 0, MPI_INT, 0, 0, MPI_COMM_WORLD);
-	 	MPI_Recv(&mensaje,1,MPI_INT,1,0, MPI_COMM_WORLD,&status); 
-	 	printf("Proceso %d  recibio %d al proceso 1", rank, mensaje );
+	 	printf("Proceso %d envia mensaje %d  al proceso 1 \n", rank, mensaje); 
+	 	MPI_Send(&mensaje, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+	 	MPI_Recv(&mensaje,1,MPI_INT,1,0, MPI_COMM_WORLD, &status); 
+	 	printf("Proceso %d  recibio %d al proceso 1\n", rank, mensaje );
     }
     if (rank ==1)
 	{ 
 		int mensaje1; 
-        MPI_Recv(&mensaje1,1,MPI_INT,0,0,MPI_COMM_WORLD, &status); 
-		printf("Proceso %d recibio  %d al proceso 0 ", rank,mensaje1);
+        MPI_Recv(&mensaje1, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &status); 
+		printf("Proceso %d recibio  %d al proceso 0 \n", rank, mensaje1);
 		mensaje1+=4; 
-		MPI_Send(&mensaje1, 0, MPI_INT, 0, 0, MPI_COMM_WORLD);
+		MPI_Send(&mensaje1, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
     }
-    MPI_Finalize(); return 0;
+    MPI_Finalize(); 
+	return 0;
 }
